6eedf7f6114c13654e9b556689c52d2f4a8a0775
Sagem63 <sagem163@mail.ru>
2019-12-05 16:44:08 +0400

    sas

---
+++ Temp/gradleOut/build/generated/source/buildConfig/release/com/GG/MSG/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.GG.MSG;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.GG.MSG";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1";
+}
